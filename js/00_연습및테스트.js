fe7ce56f9071b3d9b562940076b8f67edc5bbb51
DESKTOP-RIENOSP\user1 <Sum-yujin>
2024-07-04 17:48:43 +0900

    연습

--- js/00_연습및테스트.js
+++ js/00_연습및테스트.js
@@ -13,3 +13,31 @@ function checkCount(){
 
 /** 상품 갯수 선택 및 금액 확인 */
 
+const num = document.getElementById("number");
+const price = document.getElementById("price");
+let n1 = 1;
+let p1 = 1000;
+
+num.innerText = n1;
+
+function plus(){
+  n1++;
+  num.innerText = n1;
+  price.innerHTML = "<h4> 내실 금액 : " + p1 * n1 + "원 <h4>";
+  console.log(`p클릭후: ${v1},${p1},${n1}`);
+}
+
+function minus(){
+  n1--;
+  if(n1 < 0){
+    n1 = 0;
+    return;
+  }
+  num.innerText = n1;
+  
+  console.log(`m클릭후: ${v1}`);
+}
+
+
+price.innerHTML = "<h4> 내실 금액 : " + v1 + "원 <h4>";
+
