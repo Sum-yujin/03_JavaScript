11a0b0abaf7022b23ea9d14b293c0952c0025b63
DESKTOP-RIENOSP\user1 <Sum-yujin>
2024-07-04 16:50:46 +0900

    240704_조건문,반복문 학습내용 커밋

---
+++ js/00_연습및테스트.js
@@ -0,0 +1,15 @@
+
+
+/** 글자 수 세기 */
+
+function checkCount(){
+  const input1 = document.getElementById("input1");
+  const count1 = document.getElementById("count1");
+  const count2 = document.getElementById("count2");
+
+  count1.innerText = input1.value.length;    
+}
+
+
+/** 상품 갯수 선택 및 금액 확인 */
+
